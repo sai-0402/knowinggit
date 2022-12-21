90bf04683f638bed9ea54eae0b0daed96aee26d7
sravan <114403236+sai-0402@users.noreply.github.com>
2022-12-21 09:41:14 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,1 +1,4 @@
 print("testing the new branch that we have created")
+print("why is this used beacuse while working on a project we need more branhces as a main branch contains the main application details and the other branches are used so that any change or any mistake made will not effect the main brnach")
+
+    
